04058583211cac084822632429065e3a27f3348b
Francis Nixon <13nixonf@gmail.com>
2019-09-07 16:25:04 +0100

    Added java for tests

---
+++ resources/test/simple-java-project/src/main/java/io/github/pirocks/Main.java
@@ -0,0 +1,19 @@
+package io.github.pirocks;
+
+public class Main {
+
+	public Main() {
+		long a_long_var = 1L;
+		long a_long_var_2 = 1L;
+		long other = a_long_var * a_long_var_2;
+	}
+
+	public static void main(String[] args) {
+	    System.out.println("here");
+	    System.out.println("also here");
+	    int a_var = 0;
+	    int another_var = a_var + 1;
+	    char a_char_var = 'g';
+	    double a_double  = a_var;
+    }
+}
