43ab7ee0f4bdf691d1d87609153bac046b7b9bee
Francis Nixon <fpn17@imperial.ac.uk>
2019-12-30 15:37:06 +0000

    removed a unused and unneeded commented out function declaration.

--- verification/src/verifier/mod.rs
+++ verification/src/verifier/mod.rs
@@ -109,8 +109,6 @@ pub struct FieldDescriptor {
 
 pub enum Descriptor {}
 
-//fn modify_local_variable() //todo
-
 fn passes_protected_check(env: &Environment, member_class_name: String, _member_name: String/*, _member_descriptor: !*/, _stack_frame: &Frame) -> Result<(), TypeSafetyError> {
     let mut chain = vec![];
     super_class_chain(env.method.prolog_class, env.class_loader.clone(), &mut chain)?;//todo is this strictly correct?
