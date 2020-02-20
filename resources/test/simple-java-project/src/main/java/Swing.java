37b78d3d3568bc4baf64dc59ba6e2a214f3e633a
Francis Nixon <13nixonf@gmail.com>
2020-02-20 14:19:45 +0000

    add Swing.class

---
+++ resources/test/simple-java-project/src/main/java/Swing.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Swing {
+    public static void main(String[] args) {
+        JFrame test = new JFrame("test");
+        test.setSize(100,100);
+        test.setVisible(true);
+    }
+}
