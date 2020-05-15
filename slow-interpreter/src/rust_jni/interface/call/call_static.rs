b6772c2f76f4d8ba26bbb8f7d2560e8c542e68f8
Francis Nixon <13nixonf@gmail.com>
2020-05-16 00:42:44 +0100

    impl call_static_*_method

--- slow-interpreter/src/rust_jni/interface/call/call_static.rs
+++ slow-interpreter/src/rust_jni/interface/call/call_static.rs
@@ -68,10 +68,58 @@ pub unsafe extern "C" fn call_static_object_method(env: *mut JNIEnv, _clazz: jcl
     to_object(res.unwrap_object())
 }
 
-pub unsafe extern "C" fn call_static_boolean_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jobject {
+pub unsafe extern "C" fn call_static_boolean_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jboolean {
     call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
     let res = get_frame(env).pop();
-    to_object(res.unwrap_object())
+    res.unwrap_int() as jboolean
+}
+
+
+pub unsafe extern "C" fn call_static_byte_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jbyte {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_int() as jbyte
+}
+
+
+pub unsafe extern "C" fn call_static_short_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jshort {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_int() as jshort
+}
+
+
+pub unsafe extern "C" fn call_static_char_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jchar {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_int() as jchar
+}
+
+pub unsafe extern "C" fn call_static_int_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jint {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_int() as jint
+}
+
+
+pub unsafe extern "C" fn call_static_float_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jfloat {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_float()
+}
+
+
+pub unsafe extern "C" fn call_static_double_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jdouble {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_double()
+}
+
+
+pub unsafe extern "C" fn call_static_long_method(env: *mut JNIEnv, _clazz: jclass, method_id: jmethodID, mut l: ...) -> jlong {
+    call_static_method_impl(env, method_id, VarargProvider::Dots(&mut l));
+    let res = get_frame(env).pop();
+    res.unwrap_long()
 }
 
 
