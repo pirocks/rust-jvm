988651784aacf82ec3110a8bc10a7205d308041e
Francis Nixon <13nixonf@gmail.com>
2020-07-08 22:53:18 -0700

    impl exception_occured, exception_clear

--- slow-interpreter/src/rust_jni/interface/exception.rs
+++ slow-interpreter/src/rust_jni/interface/exception.rs
@@ -1,15 +1,14 @@
 use jvmti_jni_bindings::{JNIEnv, jthrowable};
 
-use crate::rust_jni::native_util::get_interpreter_state;
+use crate::rust_jni::native_util::{get_interpreter_state, to_object};
 
-pub unsafe extern "C" fn exception_occured(_env: *mut JNIEnv) -> jthrowable {
-    //exceptions don't happen yet todo
-    std::ptr::null_mut()
+pub unsafe extern "C" fn exception_occured(env: *mut JNIEnv) -> jthrowable {
+    let int_state = get_interpreter_state(env);
+    to_object(int_state.throw().clone())
 }
 
 pub unsafe extern "C" fn exception_clear(env: *mut JNIEnv) {
-    //todo not implemented yet
-    // let jvm = get_state(env);
     let int_state = get_interpreter_state(env);
+    *int_state.throw_mut() = None;
     assert!(int_state.throw_mut().is_none());
 }
