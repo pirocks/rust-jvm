b2df7e38bcdb1cd235454a05bfc04e0052041995
Francis Nixon <13nixonf@gmail.com>
2020-07-24 04:05:21 -0700

    impl get_field_modifiers

--- slow-interpreter/src/jvmti/mod.rs
+++ slow-interpreter/src/jvmti/mod.rs
@@ -399,28 +399,7 @@ pub mod properties;
 pub mod allocate;
 pub mod events;
 
-pub mod field {
-    use std::intrinsics::transmute;
-    use std::sync::Arc;
-
-    use classfile_parser::code::InstructionTypeNum::l2d;
-    use classfile_view::view::HasAccessFlags;
-    use jvmti_jni_bindings::{jboolean, jclass, jfieldID, jvmtiEnv, jvmtiError, jvmtiError_JVMTI_ERROR_NONE};
-
-    use crate::field_table::FieldId;
-    use crate::jvmti::get_state;
-    use crate::rust_jni::native_util::from_jclass;
-
-    pub unsafe extern "C" fn is_field_synthetic(env: *mut jvmtiEnv, klass: jclass, field: jfieldID, is_synthetic_ptr: *mut jboolean) -> jvmtiError {
-        let jvm = get_state(env);
-        let field_id: FieldId = unsafe { transmute(field) };
-        let (runtime_class, i) = jvm.field_table.read().unwrap().lookup(field_id);
-        Arc::ptr_eq(&from_jclass(klass).as_runtime_class(), &runtime_class);
-        let is_synthetic = runtime_class.view().field(i as usize).is_synthetic();
-        is_synthetic_ptr.write(is_synthetic as jboolean);
-        jvmtiError_JVMTI_ERROR_NONE
-    }
-}
+pub mod field;
 
 pub mod locals;
 
