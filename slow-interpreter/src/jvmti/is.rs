caeafdf4cebb90c9e561bc9e0a4bb3eea73199d8
Francis Nixon <13nixonf@gmail.com>
2020-09-10 02:06:18 -0700

    fix/cleanup/document is_method_obsolete

--- slow-interpreter/src/jvmti/is.rs
+++ slow-interpreter/src/jvmti/is.rs
@@ -10,9 +10,9 @@ use crate::rust_jni::native_util::from_object;
 
 pub unsafe extern "C" fn is_array_class(env: *mut jvmtiEnv, klass: jclass, is_array_class_ptr: *mut jboolean) -> jvmtiError {
     let jvm = get_state(env);
-    let tracign_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsArrayClass");
+    let tracing_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsArrayClass");
     is_array_class_ptr.write(is_array_impl(klass));
-    jvm.tracing.trace_jdwp_function_exit(tracign_guard, jvmtiError_JVMTI_ERROR_NONE)
+    jvm.tracing.trace_jdwp_function_exit(tracing_guard, jvmtiError_JVMTI_ERROR_NONE)
 }
 
 pub fn is_array_impl(cls: jclass) -> u8 {
@@ -24,18 +24,44 @@ pub fn is_array_impl(cls: jclass) -> u8 {
 
 pub unsafe extern "C" fn is_interface(env: *mut jvmtiEnv, klass: jclass, is_interface_ptr: *mut jboolean) -> jvmtiError {
     let jvm = get_state(env);
-    let tracign_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsInterface");
+    let tracing_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsInterface");
     let res = from_object(transmute(klass)).unwrap().unwrap_normal_object().class_pointer.view().is_interface();
     is_interface_ptr.write(res as u8);
-    jvm.tracing.trace_jdwp_function_exit(tracign_guard, jvmtiError_JVMTI_ERROR_NONE)
+    jvm.tracing.trace_jdwp_function_exit(tracing_guard, jvmtiError_JVMTI_ERROR_NONE)
 }
 
-
+///Is Method Obsolete
+///
+///     jvmtiError
+///     IsMethodObsolete(jvmtiEnv* env,
+///                 jmethodID method,
+///                 jboolean* is_obsolete_ptr)
+///
+/// Determine if a method ID refers to an obsolete method version.
+///
+/// Phase	Callback Safe	Position	Since
+/// may only be called during the start or the live phase 	No 	91	1.0
+///
+/// Capabilities
+/// Required Functionality
+///
+/// Parameters
+/// Name 	Type 	Description
+/// method	jmethodID	The method ID to query.
+/// is_obsolete_ptr	jboolean*	On return, points to the boolean result of this function.
+///
+/// Agent passes a pointer to a jboolean. On return, the jboolean has been set.
+///
+/// Errors
+/// This function returns either a universal error or one of the following errors
+/// Error 	Description
+/// JVMTI_ERROR_INVALID_METHODID	method is not a jmethodID.
+/// JVMTI_ERROR_NULL_POINTER	is_obsolete_ptr is NULL.
 pub unsafe extern "C" fn is_method_obsolete(env: *mut jvmtiEnv, _method: jmethodID, is_obsolete_ptr: *mut jboolean) -> jvmtiError {
     let jvm = get_state(env);
-    let tracign_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsMethodObsolete");
+    let tracing_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsMethodObsolete");
     is_obsolete_ptr.write(false as u8);//todo don't support retransform classes.
-    jvm.tracing.trace_jdwp_function_exit(tracign_guard, jvmtiError_JVMTI_ERROR_NONE)
+    jvm.tracing.trace_jdwp_function_exit(tracing_guard, jvmtiError_JVMTI_ERROR_NONE)
 }
 
 
