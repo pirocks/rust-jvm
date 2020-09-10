84a15eb6687a18c369ad4edd79367596be9b5a1f
Francis Nixon <13nixonf@gmail.com>
2020-09-10 02:36:23 -0700

    fix/cleanup/document is_array_class

--- slow-interpreter/src/jvmti/is.rs
+++ slow-interpreter/src/jvmti/is.rs
@@ -9,18 +9,52 @@ use crate::jvmti::get_state;
 use crate::method_table::MethodId;
 use crate::rust_jni::native_util::{from_jclass, from_object, try_from_jclass};
 
+///Is Array Class
+///
+///     jvmtiError
+///     IsArrayClass(jvmtiEnv* env,
+///                 jclass klass,
+///                 jboolean* is_array_class_ptr)
+///
+/// Determines whether a class object reference represents an array. The jboolean result is JNI_TRUE if the class is an array, JNI_FALSE otherwise.
+///
+/// Phase	Callback Safe	Position	Since
+/// may only be called during the start or the live phase 	No 	56	1.0
+///
+/// Capabilities
+/// Required Functionality
+///
+/// Parameters
+/// Name 	Type 	Description
+/// klass	jclass	The class to query.
+/// is_array_class_ptr	jboolean*	On return, points to the boolean result of this function.
+///
+/// Agent passes a pointer to a jboolean. On return, the jboolean has been set.
+///
+/// Errors
+/// This function returns either a universal error or one of the following errors
+/// Error 	Description
+/// JVMTI_ERROR_INVALID_CLASS	klass is not a class object or the class has been unloaded.
+/// JVMTI_ERROR_NULL_POINTER	is_array_class_ptr is NULL.
 pub unsafe extern "C" fn is_array_class(env: *mut jvmtiEnv, klass: jclass, is_array_class_ptr: *mut jboolean) -> jvmtiError {
     let jvm = get_state(env);
     let tracing_guard = jvm.tracing.trace_jdwp_function_enter(jvm, "IsArrayClass");
-    is_array_class_ptr.write(is_array_impl(klass));
+    let res = match is_array_impl(klass) {
+        Ok(res) => res,
+        Err(err) => return jvm.tracing.trace_jdwp_function_exit(tracing_guard, err)
+    };
+    is_array_class_ptr.write(res);
     jvm.tracing.trace_jdwp_function_exit(tracing_guard, jvmtiError_JVMTI_ERROR_NONE)
 }
 
-pub fn is_array_impl(cls: jclass) -> u8 {
-    let object_non_null = unsafe { from_object(transmute(cls)).unwrap().clone() };
-    let ptype = JavaValue::Object(object_non_null.into()).cast_class().as_type();
+pub unsafe fn is_array_impl(cls: jclass) -> Result<u8, jvmtiError> {
+    let jclass = match try_from_jclass(cls) {
+        None => return Result::Err(jvmtiError_JVMTI_ERROR_INVALID_CLASS),
+        Some(jclass) => jclass,
+    };
+    let ptype = jclass.as_type();
     let is_array = ptype.is_array();
-    is_array as jboolean
+    Result::Ok((if is_array { JNI_TRUE } else { JNI_FALSE }) as jboolean)
 }
 
 /// Is Interface
