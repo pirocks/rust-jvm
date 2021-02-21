a574f570b1e1da4eae8eef770b3df9e3241fb4cd
Francis Nixon <13nixonf@gmail.com>
2021-02-21 11:31:20 -0500

    compiler.rs not supported.

--- libjvm/src/compiler.rs
+++ libjvm/src/compiler.rs
@@ -1,37 +1,43 @@
+use std::ptr::null_mut;
+
 use jvmti_jni_bindings::{jboolean, jclass, JNIEnv, jobject, jstring};
 
 #[no_mangle]
 unsafe extern "system" fn JVM_InitializeCompiler(env: *mut JNIEnv, compCls: jclass) {
-    unimplemented!()
+    eprintln!("JVM_InitializeCompiler not supported");
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_IsSilentCompiler(env: *mut JNIEnv, compCls: jclass) -> jboolean {
-    unimplemented!()
+    eprintln!("JVM_IsSilentCompiler not supported");
+    u8::from(false)
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_CompileClass(env: *mut JNIEnv, compCls: jclass, cls: jclass) -> jboolean {
-    unimplemented!()
+    eprintln!("JVM_CompileClass not supported");
+    u8::from(false)
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_CompileClasses(env: *mut JNIEnv, cls: jclass, jname: jstring) -> jboolean {
-    unimplemented!()
+    eprintln!("JVM_CompileClasses not supported");
+    u8::from(false)
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_CompilerCommand(env: *mut JNIEnv, compCls: jclass, arg: jobject) -> jobject {
-    unimplemented!()
+    eprintln!("JVM_CompilerCommand not supported");
+    null_mut()
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_EnableCompiler(env: *mut JNIEnv, compCls: jclass) {
-    unimplemented!()
+    eprintln!("JVM_EnableCompiler not supported");
 }
 
 #[no_mangle]
 unsafe extern "system" fn JVM_DisableCompiler(env: *mut JNIEnv, compCls: jclass) {
-    unimplemented!()
+    eprintln!("JVM_DisableCompiler not supported");
 }
 
