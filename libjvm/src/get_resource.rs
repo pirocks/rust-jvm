a34975b4a852dc656a043956ab349d01468c6596
Francis Nixon <13nixonf@gmail.com>
2021-03-01 22:14:13 -0500

    impl JVM_GetResourceLookupCacheURLs JVM_GetResourceLookupCache

--- libjvm/src/get_resource.rs
+++ libjvm/src/get_resource.rs
@@ -4,11 +4,10 @@ use jvmti_jni_bindings::{jintArray, JNIEnv, jobject, jobjectArray};
 use slow_interpreter::rust_jni::interface::local_frame::new_local_ref_public;
 use slow_interpreter::rust_jni::native_util::{get_interpreter_state, to_object};
 
+//so it appears hotspot implements both of these as null.
+
 #[no_mangle]
 unsafe extern "system" fn JVM_GetResourceLookupCacheURLs(env: *mut JNIEnv, loader: jobject) -> jobjectArray {
-    // let int_state = get_interpreter_state(env);
-    // new_local_ref_public(None, int_state
-    //todo not implemented for now
     null_mut()
 }
 
@@ -19,5 +18,5 @@ unsafe extern "system" fn JVM_GetResourceLookupCache(
     loader: jobject,
     resource_name: *const ::std::os::raw::c_char,
 ) -> jintArray {
-    unimplemented!()
+    null_mut()
 }
