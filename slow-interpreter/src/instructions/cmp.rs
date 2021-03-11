48c36f31c847f94b2c91ccff43bb4191e60f524c
Francis Nixon <13nixonf@gmail.com>
2021-03-10 22:16:37 -0500

    address cmp todos.

--- slow-interpreter/src/instructions/cmp.rs
+++ slow-interpreter/src/instructions/cmp.rs
@@ -1,31 +1,37 @@
 use crate::java_values::JavaValue;
 use crate::StackEntry;
 
+//Floating-point comparison is performed in accordance with IEEE754
+// this is the same as regular rust floats
+
+
 pub fn fcmpl(current_frame: &mut StackEntry) {
     let value2 = current_frame.pop().unwrap_float();
     let value1 = current_frame.pop().unwrap_float();
-    //todo check this actually handles Nan correctly
-    if value1.to_bits() == value2.to_bits() {
-        current_frame.push(JavaValue::Int(0))
-    } else if value1 > value2 {
-        current_frame.push(JavaValue::Int(1))
-    } else if value1 < value2 {
-        current_frame.push(JavaValue::Int(-1))
-    } else {
-        current_frame.push(JavaValue::Int(-1))
+    if value1.is_nan() || value2.is_nan() {
+        current_frame.push(JavaValue::Int(-1));
+        return;
     }
+    fcmp_common(current_frame, value2, value1)
 }
 
 pub fn fcmpg(current_frame: &mut StackEntry) {
     let value2 = current_frame.pop().unwrap_float();
     let value1 = current_frame.pop().unwrap_float();
+    if value1.is_nan() || value2.is_nan() {
+        current_frame.push(JavaValue::Int(1));
+        return;
+    }
+    fcmp_common(current_frame, value2, value1)
+}
+
+fn fcmp_common(current_frame: &mut StackEntry, value2: f32, value1: f32) {
     if value1.to_bits() == value2.to_bits() {
         current_frame.push(JavaValue::Int(0))
     } else if value1 > value2 {
         current_frame.push(JavaValue::Int(1))
     } else if value1 < value2 {
         current_frame.push(JavaValue::Int(-1))
-    } else {
-        current_frame.push(JavaValue::Int(1))
-    }
+    } else { panic!() }
 }
+
