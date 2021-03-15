c834da53ce541c090e4d8e859b4d19e0189baab2
Francis Nixon <13nixonf@gmail.com>
2021-03-14 23:40:31 -0400

    fix pop2 bug

--- slow-interpreter/src/instructions/pop.rs
+++ slow-interpreter/src/instructions/pop.rs
@@ -2,10 +2,13 @@ use crate::java_values::JavaValue;
 use crate::StackEntry;
 
 pub fn pop2(current_frame: &mut StackEntry) {
-    if let _ = current_frame.pop() {
-        if let JavaValue::Long(_) | JavaValue::Double(_) = current_frame.pop() {
-            panic!()
-        };
+    match current_frame.pop() {
+        JavaValue::Long(_) | JavaValue::Double(_) => {}
+        _ => {
+            if let JavaValue::Long(_) | JavaValue::Double(_) = current_frame.pop() {
+                panic!()
+            };
+        }
     };
 }
 
