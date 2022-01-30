e8df9487e093c01c112e1907f254e85645a0b149
Francis Nixon <13nixonf@gmail.com>
2022-01-30 00:28:57 -0500

    fixed broken astore

--- slow-interpreter/src/ir_to_java_layer/compiler/local_var_stores.rs
+++ slow-interpreter/src/ir_to_java_layer/compiler/local_var_stores.rs
@@ -4,9 +4,11 @@ use crate::ir_to_java_layer::compiler::{array_into_iter, CurrentInstructionCompi
 
 pub fn astore_n(method_frame_data: &JavaCompilerMethodAndFrameData, current_instr_data: &CurrentInstructionCompilerData, n: u16) -> impl Iterator<Item=IRInstr> {
     //todo have register allocator
+    let to_offset = method_frame_data.local_var_entry(current_instr_data.current_index, n);
+    let from_offset = method_frame_data.operand_stack_entry(current_instr_data.current_index, 0);
     array_into_iter([
-        IRInstr::LoadFPRelative { from: method_frame_data.local_var_entry(current_instr_data.current_index, n), to: Register(1) },
-        IRInstr::StoreFPRelative { from: Register(1), to: method_frame_data.operand_stack_entry(current_instr_data.next_index, 0) },
+        IRInstr::LoadFPRelative { from: from_offset, to: Register(1) },
+        IRInstr::StoreFPRelative { from: Register(1), to: to_offset },
     ])
 }
 
