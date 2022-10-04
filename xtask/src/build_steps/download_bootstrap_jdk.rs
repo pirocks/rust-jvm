0ed967c632f892174d3117695bfbda6fee8838e0
Francis Nixon <13nixonf@gmail.com>
2022-10-03 20:00:12 -0400

    sync with laptop

--- xtask/src/build_steps/download_bootstrap_jdk.rs
+++ xtask/src/build_steps/download_bootstrap_jdk.rs
@@ -1,10 +1,14 @@
+use async_trait::async_trait;
 use crate::build_steps::{BuildStatus, BuildStatusValidationResult, BuildStep};
+use serde::Serialize;
+
 
 #[derive(Serialize)]
 pub struct DownloadBootstrapJDK{
 
 }
 
+#[async_trait]
 impl BuildStep for DownloadBootstrapJDK{
     async fn validate_build_status(&self, build_status: &BuildStatus) -> BuildStatusValidationResult {
         todo!()
