8c3c5e47cdf9b6c5270146dbdeee6fae996ecc19
Francis Nixon <13nixonf@gmail.com>
2022-12-12 01:01:27 -0500

    moved remote frame push into slow-interpreter.

--- slow-interpreter/src/lib.rs
+++ slow-interpreter/src/lib.rs
@@ -86,6 +86,7 @@ pub mod extra_intrinsics;
 pub mod static_vars;
 pub mod accessor_ext;
 pub mod define_class_safe;
+pub mod remote_frame_push;
 
 pub fn run_main<'gc, 'l>(args: Vec<String>, jvm: &'gc JVMState<'gc>, int_state: &mut impl PushableFrame<'gc>) -> Result<(), Box<dyn Error>> {
     let launcher = Launcher::get_launcher(jvm, int_state).expect("todo");
--- slow-interpreter/src/remote_frame_push.rs
+++ slow-interpreter/src/remote_frame_push.rs
@@ -1,17 +1,13 @@
-#![feature(box_syntax)]
-#![feature(asm_const)]
-
 use std::collections::HashMap;
 use std::ffi::{c_int, c_void};
 use std::mem::{MaybeUninit, size_of};
-use std::ops::Rem;
 use std::sync::{Arc, Mutex, RwLock};
-use std::sync::atomic::{AtomicBool};
+use std::sync::atomic::{AtomicBool, Ordering};
 #[cfg(debug_assertions)]
 use assert_no_alloc::{AllocDisabler};
 use assert_no_alloc::assert_no_alloc;
 use nix::libc;
-use nix::libc::{REG_RDI, REG_RIP, REG_RSP, siginfo_t, ucontext_t};
+use nix::libc::{greg_t, REG_RBP, REG_RDI, REG_RIP, REG_RSP, setcontext, siginfo_t, ucontext_t};
 use nix::sys::pthread::{Pthread, pthread_sigqueue, SigVal};
 use nix::sys::signal::{SaFlags, sigaction, SigAction, SigHandler, Signal, SigSet};
 use nonnull_const::NonNullConst;
@@ -47,6 +43,8 @@ pub enum ThingToPush<'signal_life> {
     Data(&'signal_life [u8]),
 }
 
+pub trait RunInRemoteFramePush: for<'gc, 'k, 'l> FnOnce(&'l mut OpaqueFrame<'gc, 'k>) + Send {}
+
 #[derive(Debug)]
 pub struct RemoteQueryUnsafe<'signal_life> {
     signal_safe_data: NonNullConst<SignalAccessibleJavaStackData>,
@@ -54,6 +52,7 @@ pub struct RemoteQueryUnsafe<'signal_life> {
     register_save_area: MaybeUninit<ucontext_t>,
     new_frame_rip: *const c_void,
     okay_to_free_this: AtomicBool,
+    to_run_in_guest_frame: Box<dyn RunInRemoteFramePush>
 }
 
 impl RemoteQueryUnsafe<'_> {
@@ -80,28 +79,63 @@ pub extern "C" fn handler(sig: c_int, info: *mut siginfo_t, ucontext: *mut c_voi
     }
 }
 
+
 unsafe fn handler_impl(info: *mut siginfo_t, ucontext: Option<*const ucontext_t>) {
     if let Err(_err) = std::panic::catch_unwind(|| {
         let remote_query_pointer = info.as_ref().unwrap().si_value().sival_ptr as *mut RemoteQueryUnsafe;
         let remote_query = remote_query_pointer.as_mut().unwrap();
+        let signal_safe_data = remote_query.signal_safe_data();
+        let stack_top = signal_safe_data.stack_top;
+        let stack_bottom = signal_safe_data.stack_bottom;
+        assert!(stack_top > stack_bottom);
+
         remote_query.register_save_area = MaybeUninit::new(ucontext.unwrap().read());
         let new_rip = remote_query.new_frame_rip as u64 as i64;
         let ucontext = (ucontext.unwrap() as *mut ucontext_t).as_mut().unwrap();
         let uc_mcontext = &mut ucontext.uc_mcontext;
         let gpregs = &mut uc_mcontext.gregs;
-        let prev_rip = gpregs[REG_RIP as usize];
-        gpregs[REG_RSP as usize] &= -16;
-        gpregs[REG_RSP as usize] -= size_of::<u64>() as i64;
-        //more for debugger than actually being able to ret. return to non-handler is done with setcontext
-        let stack = gpregs[REG_RSP as usize] as *mut c_void as *mut u64;
-        stack.write(prev_rip as u64);
-        gpregs[REG_RIP as usize] = new_rip;
-        gpregs[REG_RDI as usize] = remote_query_pointer as *mut c_void as i64;
-        ucontext.uc_stack.ss_sp = stack as *mut c_void;
+        let is_in_guest = in_guest(stack_top, stack_bottom, gpregs);
+        if is_in_guest{
+            let prev_rip = gpregs[REG_RIP as usize];
+            gpregs[REG_RSP as usize] &= -16;
+            gpregs[REG_RSP as usize] -= size_of::<u64>() as i64;
+            //more for debugger than actually being able to ret. return to non-handler is done with setcontext
+            let stack = gpregs[REG_RSP as usize] as *mut c_void as *mut u64;
+            stack.write(prev_rip as u64);
+            gpregs[REG_RIP as usize] = new_rip;
+            gpregs[REG_RDI as usize] = remote_query_pointer as *mut c_void as i64;
+            ucontext.uc_stack.ss_sp = stack as *mut c_void;
+        }else {
+            todo!()
+        }
     }) {
         eprintln!("panic in signal handler");
         libc::abort();
     }
+}
+
+fn in_guest(stack_top: *const c_void, stack_bottom: *const c_void, gpregs: &[greg_t; 23]) -> bool {
+    let stack_pointer = gpregs[REG_RSP as usize];
+    let frame_pointer = gpregs[REG_RBP as usize];
+    let instruction_pointer = gpregs[REG_RIP as usize];
+    let stack_pointer_in_stack = (stack_pointer as *const c_void) < stack_top && (stack_pointer as *const c_void) > stack_bottom;
+    let frame_pointer_in_stack = (frame_pointer as *const c_void) < stack_top && (frame_pointer as *const c_void) > stack_bottom;
+    let in_guest = stack_pointer_in_stack && frame_pointer_in_stack;
+    let transitioning = stack_pointer_in_stack ^ frame_pointer_in_stack;
+    in_guest
+}
+
+//need to make extra room on the stack for locking stack frame, below this method.
+#[no_mangle]
+unsafe extern "C" fn no_longer_in_handler(query: *mut RemoteQueryUnsafe) {
+    let saved = libc::__errno_location().read();
+    println!("not in handler");
+    let all_registers_restore = query.as_ref().unwrap().register_save_area.clone();
+    let all_registers_restore_ptr = all_registers_restore.assume_init_ref().clone();
+    query.as_ref().unwrap().okay_to_free_this.store(true, Ordering::SeqCst);
+    libc::__errno_location().write(saved);
+    setcontext(&all_registers_restore_ptr);
+    unreachable!();
 }
 
 
@@ -114,8 +148,11 @@ impl RemoteFramePush {
         self.per_thread_signal_lock.write().unwrap().entry(tid).or_default().clone()
     }
 
-    fn send_signal(&self, tid: Pthread, data: *mut RemoteQueryUnsafe) {
+    fn send_signal(&self, tid: Pthread, data: *mut RemoteQueryUnsafe<>) {
+        let signal_lock = self.thread_signal_lock(tid);
+        let guard = signal_lock.lock().unwrap();
         pthread_sigqueue(tid, Some(THREAD_PAUSE_SIGNAL), SigVal::Ptr(data as *mut c_void)).unwrap();
+        drop(guard);
     }
 
     pub fn sigaction_setup() -> Self {
@@ -129,38 +166,18 @@ impl RemoteFramePush {
 
 #[cfg(test)]
 pub mod test {
-    use std::arch::asm;
     use std::ffi::c_void;
     use std::hint::spin_loop;
     use std::mem::MaybeUninit;
-    use std::ops::Rem;
     use std::ptr::null_mut;
     use std::sync::atomic::{AtomicBool, Ordering};
     use std::sync::{Arc, Barrier};
 
-    use nix::libc;
-    use nix::libc::{setcontext};
     use nix::sys::pthread::pthread_self;
     use nonnull_const::NonNullConst;
 
 
-    use crate::{RemoteFramePush, RemoteQueryUnsafe, SignalAccessibleJavaStackData};
-
-    //need to make extra room on the stack for locking stack frame, below this method.
-    #[no_mangle]
-    unsafe extern "C" fn no_longer_in_handler(query: *mut RemoteQueryUnsafe) {
-        let addr : u64;
-        asm!("mov {addr}, rsp", addr = out(reg) addr);
-        // assert_eq!(addr.rem(&16), 0);
-        let saved = libc::__errno_location().read();
-        println!("not in handler");
-        let all_registers_restore = query.as_ref().unwrap().register_save_area.clone();
-        let all_registers_restore_ptr = all_registers_restore.assume_init_ref().clone();
-        query.as_ref().unwrap().okay_to_free_this.store(true, Ordering::SeqCst);
-        libc::__errno_location().write(saved);
-        setcontext(&all_registers_restore_ptr);
-        unreachable!();
-    }
+    use crate::{no_longer_in_handler, RemoteFramePush, RemoteQueryUnsafe, SignalAccessibleJavaStackData};
 
     #[test]
     pub fn test() {
@@ -171,6 +188,7 @@ pub mod test {
             okay_to_free_this: AtomicBool::new(false),
             new_frame_rip: no_longer_in_handler as *const c_void,
             register_save_area: MaybeUninit::uninit(),
+            to_run_in_guest_frame: Box::new(todo!()),
         };
         let barrier = Arc::new(Barrier::new(2));
         let barrier_clone = barrier.clone();
